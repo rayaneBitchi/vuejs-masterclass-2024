4f7298cc76c9e37ab30d6e0b815e2d6ee6267352
rayaneBitchi <rayanebitchi@icloud.com>
2024-08-18 19:48:26 -0400

    Bug fixed

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import type { Database } from '../../database/types'
 
 // Create a single supabase client for interacting with your database
+
 const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
